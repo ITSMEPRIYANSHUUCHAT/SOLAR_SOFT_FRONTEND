a466c1b7f77105560ca0c632ec984bf9d1d8560f
PRIYANSHU Uchat <priyanshu.uc507@gmail.com>
2025-07-31 00:07:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 8080,
     proxy: {
       '/api': {
         target: 'http://backend:5000',
